4bec412ea124ba645d79bd9be12c8d6c8924dc89
MrHiGamer <mrhi53072@gmail.com>
2025-07-26 13:54:33 -0700

    .
    Added 2 squares 😭

---
+++ 2d rythm roguelite/Assets/Scripts/TimingHandler.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+public class TimingHandler : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+    //public Text PressButtonLabel;
+    // Update is called once per frame
+    void Update()
+    {
+        //if (InputSystem.IsPressed(Keyboard[Key.space])) 
+        //{
+           //PressButtonLabel.text = "Key Pressed";
+        //}
+    }
+}
