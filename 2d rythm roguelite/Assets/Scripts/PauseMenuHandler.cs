91edf03331a04b9d6150ca9528264219243ddeb5
MrHiGamer <mrhi53072@gmail.com>
2025-08-19 21:25:31 -0700

    hting

--- 2d rythm roguelite/Assets/Scripts/PauseMenuHandler.cs
+++ 2d rythm roguelite/Assets/Scripts/PauseMenuHandler.cs
@@ -1,14 +1,22 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 public class PauseMenuHandler : MonoBehaviour
 {
     
-   public GameObject PauseMenu;
+    public Canvas PauseMenuCanvas;
     private bool isShowing = false;
     
-    public void PauseTheMenu(){
-        isShowing = !isShowing;
-        PauseMenu.SetActive(isShowing);
+    public void HidePauseMenu()
+    {
+        PauseMenuCanvas.enabled = false;
+        isShowing = false;
     }
     
+    public void PauseTheMenu(){
+        PauseMenuCanvas.enabled = !isShowing;
+        isShowing = !isShowing;
+    }
+
+ 
 }
